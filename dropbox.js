94e1f330a48d29f45c21a0c3a7dea9272ebe7832
tomas <tomaspavlin@notebook.com>
2016-09-22 17:49:28 +0300

    buttons added, account info feature added

--- dropbox.js
+++ dropbox.js
@@ -1,5 +1,6 @@
 var dropbox = (function(){
 	var CLIENT_ID = "5tssbcxxu7f3w8t"
+	var AUTH_URL = "https://www.dropbox.com/1/oauth2/authorize?";
 
 	/* returns the access token if available (already authorized), in other case returns undefined */
 	var getToken = function(){
@@ -14,7 +15,7 @@ var dropbox = (function(){
 	/* redirects to dropbox auth url with the callers redirection url */
 	var authorize = function(){
 		var redirect_uri = document.location.href.split('?')[0];
-		
+
 		var params = {
 			'response_type': 'token',
 			'client_id' : CLIENT_ID,
@@ -24,15 +25,34 @@ var dropbox = (function(){
 
 		var p = $.param(params);
 
-		var url = "https://www.dropbox.com/1/oauth2/authorize?" + p;
+		var url = AUTH_URL + p;
 		document.location.href = url;
 	};
+
+	var getAccountInfo = function(callback){
+		var p = {
+			'access_token': getToken()
+		};
+
+		$.get('https://api.dropboxapi.com/1/account/info', p, function(data){
+			// data is string, not object
+			callback(data);
+		});
+	};
+
+	var uploadImage = function(img){
+		// TODO: upload image to dropbox
+	};
+
+
 
 
 	return {
 		getToken: getToken,
 		isTokenAvailable: isTokenAvailable,
-		authorize: authorize
+		authorize: authorize,
+		getAccountInfo: getAccountInfo,
+		uploadImage: uploadImage
 	};
 
 })();
